49fc211fb4d3579b90841cd1b0df78d3b8215423
DeZerom <demid2001s@gmail.com>
2026-01-24 16:54:15 +0300

    make system bars for dark theme

--- composeApp/src/androidMain/kotlin/ru/kabanchik/client/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/ru/kabanchik/client/MainActivity.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.view.WindowCompat
 import com.arkivanov.decompose.defaultComponentContext
 import ru.kabanchik.client.component.RootComponent
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
+
+        enableEdgeToEdge()
+        val insetsController = WindowCompat.getInsetsController(window, window.decorView)
+        insetsController.isAppearanceLightStatusBars = false
+        insetsController.isAppearanceLightNavigationBars = false
 
         val rootComponent = RootComponent.create(
             componentContext = defaultComponentContext()
